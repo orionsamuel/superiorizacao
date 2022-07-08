8263e640865da6ff0293d5066db337a8e3b799f1
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-07-08 00:36:25 -0300

    Retirando comentários

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,7 +8,7 @@ const string inputGas = "../Input/gas.txt";
 const string simulationFile = "../Input/SPE1CASE1.DATA";
 const string fileName = "SPE1CASE1";
 
-#define N_ITERATIONS 3
+#define N_ITERATIONS 10
 #define STOP 80
 
 #define MIN_POROSITY 0.1
@@ -29,7 +29,7 @@ const string fileName = "SPE1CASE1";
 
 #define HEIGHT 30
 #define WIDTH 10
-#define SIZE 10
+#define SIZE 30
 #define TABU_SIZE 30
 #define SUPERIOZATION_SIZE 8
 
